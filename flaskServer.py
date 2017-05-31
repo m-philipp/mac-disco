17bc80fff63e2ca72fb715c566933be203bd06d3
Martin Philipp <mail@martin-philipp.de>
2017-06-01 01:21:22 +0200

    o

--- flaskServer.py
+++ flaskServer.py
@@ -11,6 +11,21 @@ app = Flask(__name__)
 def index():
 	return redirect(url_for('static', filename='index.html'))
 
+@app.route('/getActiveNames')
+def getActiveNames():
+	timestamp = time.time() - 30*60 # 30 Minutes ago
+	query = 'SELECT name2macs.name, MAX(seenMacs.timestamp) as timestamp FROM seenMacs INNER JOIN name2macs on seenMacs.mac = name2macs.mac WHERE timestamp>'+timestamp+' GROUP BY name2macs.name ORDER BY seenMacs.timestamp DESC'
+	# results in:
+	# hans | 1496271433
+	# peter | 1496201475
+	
+	conn = sqlite3.connect("macStore.db")
+	c = conn.cursor()
+	c.execute(query)
+	rows = c.fetchall()
+	conn.close()
+	return jsonify(rows)
+
 @app.route('/saveName')
 def saveName():
 	remoteIp = request.remote_addr;
